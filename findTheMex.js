fb88580b472ebe8c499c6c3157dfd387a2712073
kiri23 <christian@opticpower.com>
2024-11-08 22:37:11 -0400

    add comment to find the mex example

--- findTheMex.js
+++ findTheMex.js
@@ -1,15 +1,19 @@
+/**
+ * find the Minimun excluded (Mex) non negative number that is not in the array.
+ * Solution:
+ *  Sort the array, find the gap in the array and return it
+ * @param {*} arr
+ * @returns the mex
+ */
 function findTheMex(arr) {
   const sortedArray = arr.sort((a, b) => a - b);
-  console.log("arr sorted", sortedArray);
   if (sortedArray.length === 0 || sortedArray[0] != 0) {
     return 0;
   }
   const gap = sortedArray.find((value, idx, arr) => {
     if (value < 0) {
-      return;
+      return false;
     }
-    console.log("value", value);
-    console.log(arr[idx + 1]);
     return arr[idx + 1] - value > 1;
   });
   if (!gap) {
@@ -18,7 +22,5 @@ function findTheMex(arr) {
   return gap + 1;
 }
 
-// sort , find the minimun - encuentra el gap despues del sort
-// pero tienes que skipear los negativos
 const mex = findTheMex([0, 1, 2, 3, -100, -200]);
 console.log("mex", mex);
